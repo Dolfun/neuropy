928ba2bbfa506c20c5287f9e9d22acfe677a6e64
Dolfun <abhinav150104@gmail.com>
2023-03-25 04:02:11 +0530

    Added barzilai_borwein_learning_rate

--- neuropy/gradient_descent.py
+++ neuropy/gradient_descent.py
@@ -27,5 +27,13 @@ def gradient_descent(x, y, w_in, *, alpha_init=1.0,
         if i % np.ceil(nr_iterations / nr_output) == 0:
             print(f'Iteration {i:4d}: Cost {cost_history[-1]:8.5f}')
 
+        if np.allclose(grad, prev_grad):
+            break
+
     return w, cost_history, alpha_history
-    
+
+
+def barzilai_borwein_learning_rate(w, prev_w, grad, prev_grad):
+    w_diff = w - prev_w
+    grad_diff = grad - prev_grad
+    return np.dot(w_diff, grad_diff) / np.dot(grad_diff, grad_diff)
