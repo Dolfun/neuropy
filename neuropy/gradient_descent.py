cbece6501e72f86d87c8a2ddc1ce8c4d7289d40b
Dolfun <abhinav150104@gmail.com>
2023-03-25 04:14:12 +0530

    Renamed functions

--- neuropy/gradient_descent.py
+++ neuropy/gradient_descent.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 def gradient_descent(x, y, w_in, *, alpha_init=1.0,
-                     calculate_gradient, calculate_cost, calculate_learning_rate,
+                     compute_gradient, compute_cost, compute_learning_rate,
                      nr_iterations, nr_output=10):
 
     w = np.copy(w_in)
@@ -13,15 +13,15 @@ def gradient_descent(x, y, w_in, *, alpha_init=1.0,
     prev_w = prev_grad = np.zeros(w.shape)
 
     for i in range(nr_iterations):
-        grad = calculate_gradient(x, y, w)
-        alpha = calculate_learning_rate(w, prev_w, grad, prev_grad) if i != 0 else alpha_init
+        grad = compute_gradient(x, y, w)
+        alpha = compute_learning_rate(w, prev_w, grad, prev_grad) if i != 0 else alpha_init
 
         prev_w = np.copy(w)
         prev_grad = np.copy(grad)
 
         w -= alpha * grad
 
-        cost_history.append(calculate_cost(x, y, w))
+        cost_history.append(compute_cost(x, y, w))
         alpha_history.append(alpha)
 
         if i % np.ceil(nr_iterations / nr_output) == 0:
