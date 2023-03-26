5b672b6f62cf00370028c5189c806911447f33ac
Dolfun <abhinav150104@gmail.com>
2023-03-26 23:57:05 +0530

    Fixed a bug

--- neuropy/linear_regression.py
+++ neuropy/linear_regression.py
@@ -10,7 +10,7 @@ def linear_regression_regularized_analytical_solution(x, y, _lambda):
     return np.linalg.inv(x.T @ x + _lambda * n * np.eye(p)) @ x.T @ y
 
 
-def generate_power_set(n, s, curr_list, *, power_set):
+def _generate_power_set(n, s, curr_list, power_set):
     if n == 1:
         curr_list.append(s)
         power_set.append(curr_list)
@@ -19,14 +19,18 @@ def generate_power_set(n, s, curr_list, *, power_set):
         if i <= s:
             new_list = curr_list.copy()
             new_list.append(i)
-            generate_power_set(n - 1, s - i, new_list, power_set=power_set)
+            _generate_power_set(n - 1, s - i, new_list, power_set=power_set)
+
+
+def generate_power_set(n, s, power_set):
+    _generate_power_set(n, s, [], power_set)
 
 
 def generate_polynomial_features(x_in, power_set):
     n, p = x_in.shape
     x = np.ones((n, len(power_set)))
     for i, curr_set in enumerate(power_set):
-        for j in range(n):
+        for j in range(p):
             x[:, i] *= np.power(x_in[:, j], curr_set[j])
 
     return x
