260c51acdcbdce9340ac8ed394368b01f7432e3a
Dolfun <abhinav150104@gmail.com>
2023-03-26 18:44:39 +0530

    Added polynomial feature generation

--- neuropy/linear_regression.py
+++ neuropy/linear_regression.py
@@ -9,3 +9,25 @@ def linear_regression_regularized_analytical_solution(x, y, _lambda):
     n, p = x.shape
     return np.linalg.inv(x.T @ x + _lambda * n * np.eye(p)) @ x.T @ y
 
+
+def generate_power_set(n, s, curr_list, *, power_set):
+    if n == 1:
+        curr_list.append(s)
+        power_set.append(curr_list)
+        return
+    for i in range(s + 1):
+        if i <= s:
+            new_list = curr_list.copy()
+            new_list.append(i)
+            generate_power_set(n - 1, s - i, new_list, power_set=power_set)
+
+
+def generate_polynomial_features(x_in, power_set):
+    n, p = x_in.shape
+    x = np.ones((n, len(power_set)))
+    for i, curr_set in enumerate(power_set):
+        for j in range(n):
+            x[:, i] *= np.power(x_in[:, j], curr_set[j])
+
+    return x
+
